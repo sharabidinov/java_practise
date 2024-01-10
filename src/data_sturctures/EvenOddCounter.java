158623c90bbe6bcf7802aefe2098b6ab349cae94
Alymbek Sharabidinov <73030640+sharabidinov@users.noreply.github.com>
2024-01-10 21:33:26 +0600

    Задача №112279. Чётные и нечётные

---
+++ src/data_sturctures/EvenOddCounter.java
@@ -0,0 +1,29 @@
+package data_sturctures;
+
+import java.util.Scanner;
+
+public class EvenOddCounter {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int n = sc.nextInt();
+        sc.close();
+        int[] array = new int[n];
+        int evenCounter = 0;
+        int oddCounter = 0;
+        for (int i = 0; i < array.length; i++) {
+            array[i] = (int) (Math.random() * ((b + 1) - a)) + a;
+            if (array[i] % 2 == 0) {
+                evenCounter++;
+            } else {
+                oddCounter++;
+            }
+        }
+        for (int item : array) {
+            System.out.print(item + " ");
+        }
+        System.out.println();
+        System.out.println(evenCounter + " " + oddCounter);
+    }
+}
