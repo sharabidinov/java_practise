9fd7829a8a8268611e94761f6258c87c26938f8b
Alymbek Sharabidinov <73030640+sharabidinov@users.noreply.github.com>
2024-01-11 19:43:40 +0600

    Задача №112280. Подсчёт по сумме цифр

---
+++ src/data_sturctures/CountDigitSum.java
@@ -0,0 +1,35 @@
+package data_sturctures;
+
+import java.util.Scanner;
+
+public class CountDigitSum {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int n = sc.nextInt();
+        int k = sc.nextInt();
+        sc.close();
+        int[] array = new int[n];
+
+        for (int i = 0; i < array.length; i++) {
+            array[i] = (int) (Math.random() * ((b + 1) - a)) + a;
+        }
+        int counter = 0;
+
+        for (int item : array) {
+            int sum = 0;
+            System.out.print(item + " ");
+            while (item != 0) {
+                sum += item % 10;
+                item /= 10;
+            }
+            if (sum == k) {
+                counter++;
+            }
+        }
+        System.out.println();
+        System.out.println(counter);
+
+    }
+}
