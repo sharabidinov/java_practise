19b8fcfb98ec9af05c6d807dba320c4f40f2d0e1
Alymbek Sharabidinov <73030640+sharabidinov@users.noreply.github.com>
2024-01-13 17:38:35 +0600

    Задача №112284. Случайная перестановка

--- src/data_sturctures/arrays/RandomPermutationOfNumbers.java
+++ src/data_sturctures/arrays/RandomPermutationOfNumbers.java
@@ -4,18 +4,20 @@ import java.util.Scanner;
 
 public class RandomPermutationOfNumbers {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        int a = 1;
+        Scanner scanner = new Scanner(System.in);
+
+        int n = scanner.nextInt();
+
         int[] array = new int[n];
+        for (int i = 1; i <= n; i++) {
+            int a = 0;
+            int randomIndex = (int) (Math.random() * (i - a)) + a;
+            array[i - 1] = array[randomIndex];
+            array[randomIndex] = i;
+        }
 
         for (int i = 0; i < n; i++) {
-            int randomIndex = (int) (Math.random() * (n - a)) + a;
-            array[randomIndex] = i + 1;
-            array[i] = array[randomIndex];
-        }
-        for (int item : array) {
-            System.out.print(item + " ");
+            System.out.print(array[i] + " ");
         }
     }
 }
